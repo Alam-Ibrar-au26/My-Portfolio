73a9279de0740e25b8cf5607ebb2359ff450652c
Ibrar Alam <ibby1561@gmail.com>
2023-02-10 23:20:42 +0530

    few changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Logo from '../assets/'
+// import Logo from '../assets/'
 
 const Header = () => {
   return (
     <header className='py-8'>
       <div className='conatiner mx-auto'>
         <div className='flex justify-between items-center'>
-          <a href='#'>
-            <img src={Logo} alt />
-          </a>
           <button className='btn btn-sm'>Work with me</button>
         </div>
       </div>
