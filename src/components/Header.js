f6a3d11d67d306256d738ac512021bfd0f2e4cf8
Ibrar Alam <ibby1561@gmail.com>
2023-02-16 22:44:13 +0530

    logo & Button added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
-// import Logo from '../assets/'
+import Logo from '../assets/logo.svg';
 
 const Header = () => {
   return (
     <header className='py-8'>
-      <div className='conatiner mx-auto'>
+      <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
+          <a>
+            <img src={Logo} alt='' />
+          </a>
           <button className='btn btn-sm'>Work with me</button>
         </div>
       </div>
     </header>
-  )
+  );
 };
 
 export default Header;
